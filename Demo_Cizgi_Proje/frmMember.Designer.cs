a87a77531ecedad0bdce3b191a774a1293f0b399
MarulBtw <MarulBtw@DESKTOP-DK11B7K>
2022-02-25 10:57:14 +0300

    Proje dosyası ekle.

---
+++ Demo_Cizgi_Proje/frmMember.Designer.cs
@@ -0,0 +1,264 @@
+﻿
+namespace Demo_Cizgi_Proje
+{
+    partial class frmMember
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMemberName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMemberSurname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMemberAge = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtImageuRL = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lnkTerm = new System.Windows.Forms.LinkLabel();
+            this.chkAccept = new System.Windows.Forms.CheckBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dateBirthday = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // txtMemberName
+            // 
+            this.txtMemberName.Location = new System.Drawing.Point(136, 36);
+            this.txtMemberName.Name = "txtMemberName";
+            this.txtMemberName.Size = new System.Drawing.Size(212, 23);
+            this.txtMemberName.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Üye Adı:";
+            // 
+            // txtMemberSurname
+            // 
+            this.txtMemberSurname.Location = new System.Drawing.Point(136, 65);
+            this.txtMemberSurname.Name = "txtMemberSurname";
+            this.txtMemberSurname.Size = new System.Drawing.Size(212, 23);
+            this.txtMemberSurname.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Üye Soyad:";
+            // 
+            // txtMemberAge
+            // 
+            this.txtMemberAge.Location = new System.Drawing.Point(136, 94);
+            this.txtMemberAge.Name = "txtMemberAge";
+            this.txtMemberAge.Size = new System.Drawing.Size(212, 23);
+            this.txtMemberAge.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Yaş:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Doğum Tarihi:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(136, 152);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(212, 23);
+            this.txtUsername.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Kullanıcı Adı:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(136, 181);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(212, 23);
+            this.txtPassword.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 189);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Şifre:";
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.Location = new System.Drawing.Point(136, 210);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.Size = new System.Drawing.Size(212, 23);
+            this.txtConfirmPassword.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(26, 218);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Tekrar Şifre:";
+            // 
+            // txtImageuRL
+            // 
+            this.txtImageuRL.Location = new System.Drawing.Point(136, 239);
+            this.txtImageuRL.Name = "txtImageuRL";
+            this.txtImageuRL.Size = new System.Drawing.Size(212, 23);
+            this.txtImageuRL.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(26, 242);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 15);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Görselı:";
+            // 
+            // lnkTerm
+            // 
+            this.lnkTerm.AutoSize = true;
+            this.lnkTerm.Location = new System.Drawing.Point(133, 269);
+            this.lnkTerm.Name = "lnkTerm";
+            this.lnkTerm.Size = new System.Drawing.Size(149, 15);
+            this.lnkTerm.TabIndex = 18;
+            this.lnkTerm.TabStop = true;
+            this.lnkTerm.Text = "Kullanı Şartlarını Okudum";
+            // 
+            // chkAccept
+            // 
+            this.chkAccept.AutoSize = true;
+            this.chkAccept.Location = new System.Drawing.Point(136, 287);
+            this.chkAccept.Name = "chkAccept";
+            this.chkAccept.Size = new System.Drawing.Size(113, 19);
+            this.chkAccept.TabIndex = 19;
+            this.chkAccept.Text = "Kabul Ediyorum";
+            this.chkAccept.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(136, 312);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(212, 54);
+            this.btnSave.TabIndex = 20;
+            this.btnSave.Text = "Kaydet";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // dateBirthday
+            // 
+            this.dateBirthday.Location = new System.Drawing.Point(136, 126);
+            this.dateBirthday.Name = "dateBirthday";
+            this.dateBirthday.Size = new System.Drawing.Size(200, 23);
+            this.dateBirthday.TabIndex = 21;
+            // 
+            // frmMember
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 384);
+            this.Controls.Add(this.dateBirthday);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.chkAccept);
+            this.Controls.Add(this.lnkTerm);
+            this.Controls.Add(this.txtImageuRL);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtConfirmPassword);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMemberAge);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMemberSurname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMemberName);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "frmMember";
+            this.Text = "Yrni Üye Girişi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMemberName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMemberSurname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMemberAge;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtImageuRL;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.LinkLabel lnkTerm;
+        private System.Windows.Forms.CheckBox chkAccept;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DateTimePicker dateBirthday;
+    }
+}
